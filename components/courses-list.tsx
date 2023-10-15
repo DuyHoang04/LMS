4651febdbd7b700a930116c6b98733f5988a33aa
DuyHoang <duyhoang220904@gmail.com>
2023-10-15 23:14:22 +0700

    course-card ui

--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -1,5 +1,6 @@
 import { Category, Course } from '@prisma/client';
 import React from 'react'
+import { CourseCard } from './course-card';
 
 type CourseWithProgressWithCategory = Course & {
     category: Category | null;
@@ -16,7 +17,16 @@ export const CoursesList = ({ items }: CoursesListProps) => {
         <>
             <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
                 {items.map(item => (
-                    <h1>Hello</h1>
+                    <CourseCard
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        imageUrl={item.imageUrl!}
+                        chaptersLength={item.chapters.length}
+                        price={item.price!}
+                        progress={item.progress}
+                        category={item?.category?.name!}
+                    />
                 ))}
             </div>
             {items.length === 0 && (
