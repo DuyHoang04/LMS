1be8647c37e1915cbdef22ac93267c5728f69dbb
DuyHoang <duyhoang220904@gmail.com>
2023-10-15 23:05:14 +0700

    course-list-ui

--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -1,7 +1,29 @@
+import { Category, Course } from '@prisma/client';
 import React from 'react'
 
-export const CoursesList = () => {
+type CourseWithProgressWithCategory = Course & {
+    category: Category | null;
+    chapters: { id: string }[];
+    progress: number | null;
+};
+
+interface CoursesListProps {
+    items: CourseWithProgressWithCategory[];
+}
+
+export const CoursesList = ({ items }: CoursesListProps) => {
     return (
-        <div>course-list</div>
+        <>
+            <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+                {items.map(item => (
+                    <h1>Hello</h1>
+                ))}
+            </div>
+            {items.length === 0 && (
+                <div className="text-center text-sm text-muted-foreground mt-10">
+                    No courses found
+                </div>
+            )}
+        </>
     )
 }
